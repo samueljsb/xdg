83212cdf70a1560b0e4c4f8d7f5b4788026dc172
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2025-03-12 04:56:12 +0000

    Support `$XDG_DATA_DIRS` and `$XDG_CONFIG_DIRS`
    Resolves #1

--- xdg.py
+++ xdg.py
@@ -27,6 +27,37 @@ def data_home() -> str:
     return os.path.expanduser('~/.local/share')
 
 
+def data_dirs() -> tuple[str, ...]:
+    """Directories in which to search for user-specific data files.
+
+    This set of directories is defined by the environment variable
+    $XDG_DATA_DIRS. The directories in $XDG_DATA_DIRS should be separated with
+    a colon ':'. If $XDG_DATA_DIRS is either not set or empty, a value equal to
+    /usr/local/share/:/usr/share/ will be used.
+
+    The order of base directories denotes their importance; the first directory
+    listed is the most important. When the same information is defined in
+    multiple places the information defined relative to the more important base
+    directory takes precedent. The base directory defined by $XDG_DATA_HOME is
+    considered more important than any of the base directories defined by
+    $XDG_DATA_DIRS.
+
+    All paths set in environment variables must be absolute. If $XDG_DATA_DIRS
+    contains a relative path it will be ignored.
+
+    See <https://specifications.freedesktop.org/basedir-spec/latest/> for more
+    information.
+    """
+    xdg_data_dirs = os.getenv('XDG_DATA_DIRS')
+    if xdg_data_dirs:
+        dirs = xdg_data_dirs.split(':')
+        dirs = [dir_ for dir_ in dirs if os.path.isabs(dir_)]
+        if dirs:
+            return tuple(dirs)
+
+    return ('/usr/local/share/', '/usr/share/')
+
+
 def config_home() -> str:
     """A directory for user-specific configuration files.
 
@@ -45,6 +76,37 @@ def config_home() -> str:
         return xdg_config_home
 
     return os.path.expanduser('~/.config')
+
+
+def config_dirs() -> tuple[str, ...]:
+    """Directories in which to search for user-specific configuration files.
+
+    This set of directories is defined by the environment variable
+    $XDG_CONFIG_DIRS. The directories in $XDG_CONFIG_DIRS should be separated
+    with a colon ':'. If $XDG_CONFIG_DIRS is either not set or empty, a value
+    equal to /usr/local/share/:/usr/share/ will be used.
+
+    The order of base directories denotes their importance; the first directory
+    listed is the most important. When the same information is defined in
+    multiple places the information defined relative to the more important base
+    directory takes precedent. The base directory defined by $XDG_CONFIG_HOME
+    is considered more important than any of the base directories defined by
+    $XDG_CONFIG_DIRS.
+
+    All paths set in environment variables must be absolute. If
+    $XDG_CONFIG_DIRS contains a relative path it will be ignored.
+
+    See <https://specifications.freedesktop.org/basedir-spec/latest/> for more
+    information.
+    """
+    xdg_config_dirs = os.getenv('XDG_CONFIG_DIRS')
+    if xdg_config_dirs:
+        dirs = xdg_config_dirs.split(':')
+        dirs = [dir_ for dir_ in dirs if os.path.isabs(dir_)]
+        if dirs:
+            return tuple(dirs)
+
+    return ('/etc/xdg',)
 
 
 def state_home() -> str:
--- xdg_test.py
+++ xdg_test.py
@@ -42,6 +42,50 @@ class TestDataHome:
         assert data_home == '/home/my-user/.local/share'
 
 
+class TestDataDirs:
+    def test_read_from_environment(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ, {'XDG_DATA_DIRS': '/my-data:/other-data'}, clear=True
+        ):
+            data_dirs = xdg.data_dirs()
+
+        assert data_dirs == ('/my-data', '/other-data')
+
+    def test_default_if_empty(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ, {'XDG_DATA_DIRS': ''}, clear=True
+        ):
+            data_dirs = xdg.data_dirs()
+
+        assert data_dirs == ('/usr/local/share/', '/usr/share/')
+
+    def test_default_if_not_set(self) -> None:
+        with unittest.mock.patch.dict(os.environ, {}, clear=True):
+            data_dirs = xdg.data_dirs()
+
+        assert data_dirs == ('/usr/local/share/', '/usr/share/')
+
+    def test_relative_paths_ignored(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ,
+            {'XDG_DATA_DIRS': './relative-data:/other-data'},
+            clear=True,
+        ):
+            data_dirs = xdg.data_dirs()
+
+        assert data_dirs == ('/other-data',)
+
+    def test_default_if_all_relative_paths(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ,
+            {'XDG_DATA_DIRS': './relative-data:./other-relative-data'},
+            clear=True,
+        ):
+            data_dirs = xdg.data_dirs()
+
+        assert data_dirs == ('/usr/local/share/', '/usr/share/')
+
+
 class TestConfigHome:
     def test_read_from_environment(self) -> None:
         with unittest.mock.patch.dict(
@@ -78,6 +122,52 @@ class TestConfigHome:
             config_home = xdg.config_home()
 
         assert config_home == '/home/my-user/.config'
+
+
+class TestConfigDirs:
+    def test_read_from_environment(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ,
+            {'XDG_CONFIG_DIRS': '/my-config:/other-config'},
+            clear=True,
+        ):
+            config_dirs = xdg.config_dirs()
+
+        assert config_dirs == ('/my-config', '/other-config')
+
+    def test_default_if_empty(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ, {'XDG_CONFIG_DIRS': ''}, clear=True
+        ):
+            config_dirs = xdg.config_dirs()
+
+        assert config_dirs == ('/etc/xdg',)
+
+    def test_default_if_not_set(self) -> None:
+        with unittest.mock.patch.dict(os.environ, {}, clear=True):
+            config_dirs = xdg.config_dirs()
+
+        assert config_dirs == ('/etc/xdg',)
+
+    def test_relative_paths_ignored(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ,
+            {'XDG_CONFIG_DIRS': './relative-config:/other-config'},
+            clear=True,
+        ):
+            config_dirs = xdg.config_dirs()
+
+        assert config_dirs == ('/other-config',)
+
+    def test_default_if_all_relative_paths(self) -> None:
+        with unittest.mock.patch.dict(
+            os.environ,
+            {'XDG_CONFIG_DIRS': './relative-config:./other-relative-config'},
+            clear=True,
+        ):
+            config_dirs = xdg.config_dirs()
+
+        assert config_dirs == ('/etc/xdg',)
 
 
 class TestStateHome:
